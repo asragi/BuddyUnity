f29024905cdc84c32e3e894ff36377ce1a270fbc
asRagi <ragi-justaway@hotmail.co.jp>
2020-11-13 14:21:40 +0900

    initial commit

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BuddyDomain;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Class1.Test);
+    }
+
+    public static string Sample() => "Sample";
+}
